b14acd251ebb8fd52fa44892008114e5729331e1
maximgrinin <maximgrinin@gmail.com>
2022-11-03 11:15:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+public class Main {
+    public static void main(String[] args) {
+        // ClassWork();
+
+        // Level1:
+        // Астерикс и Обеликс варили зелье.
+        // Зелье будет сварено правильно, если его будут варить больше 5 часов.
+        // Астерикс варил его 3 часа, Обеликс в два раза меньше.
+        // Получится ли у них сварить зелье?
+        // true - если получится, иначе false
+        double neededCookTime = 5;
+        double currentCookTime;
+
+        currentCookTime = 3;                    // Вариал Астерикс
+        currentCookTime += currentCookTime / 2; // Доваривал Обеликс
+
+        if (currentCookTime > neededCookTime) {
+            System.out.println("Получится сварить зелье");
+        } else {
+            System.out.println("Не получится сварить зелье :(");
+        }
+
+        // Level2:
+        // Феликс прожил 3_011_456_567 секунд.
+        // Сколько лет прожил Феликс?
+        // Посчитать, используя Java, Long-переменные и деление.
+        long ttlInSeconds = 3_011_456_567L;
+
+        // В минуты, в часы, в дни, в годы (в одной секунде - 1/31_536_000 года)
+        double ttlInYears = (double) ttlInSeconds / 60 / 60 / 24 / 365;
+        System.out.printf("Феликс прожил %.0f лет (без учета високосных годов) и еще %.0f месяцев\n",
+                ttlInYears, Math.floor(ttlInYears % 1 * 12));
+    }
+
+    public static void ClassWork() {
+
+        // Вася получает зарплату в 5.6 евро в час. Он работает 12 часов в день,
+        // у него нет выходных, кроме 31 декабря. Он работает 364 дня в году.
+        // Ему дали отпуск в течение двух недель, он оплачивается в 50% от зп, пересчитайте зарплату за год
+        double annualSalary;
+        double perHour = 5.6;
+        int hoursPerDay = 12;
+        int daysPerYear = 364;
+        int vacationPeriod = 14;
+
+        annualSalary = perHour * hoursPerDay * (daysPerYear - vacationPeriod);
+        annualSalary += perHour / 2 * hoursPerDay * vacationPeriod;
+
+        System.out.printf("Годовая зарплата Васи: %.2f\n", annualSalary);
+
+        // Мама дала Васе 1000 рублей, у него осталось 1000 рублей после похода в магазин.
+        // Узнать, купил ли что-то Вася. Если купил - выводим true.
+        // Если не купил - выводим false.
+        double givenMoney = 1000;
+        double restMoney = 1000;
+        if (restMoney < givenMoney) {
+            System.out.println("Купил");
+        } else {
+            System.out.println("Не купил");
+        }
+
+        // Петя имеет средний балл 3.49. Он очень хочет пятерку за четверть.
+        // Он уверен, что сможет улучить средний балл на +1 (Подсказка: будет 4.49).
+        // Получит ли он 5? true - если да, false - если нет.
+        double avgGrade = 3.49;
+        double wishGrade = avgGrade + 1;
+
+        if (wishGrade >= 4.5) {
+            System.out.println("Получит");
+        } else {
+            System.out.println("Не получит");
+        }
+        if (Math.round(wishGrade) == 5) {
+            System.out.println("Получит");
+        } else {
+            System.out.println("Не получит");
+        }
+    }
+}
